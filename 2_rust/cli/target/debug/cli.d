8ec19ed572dcf819faef829849b546b4289e68e1
Mario Lazzari <mario.lazzari@gmail.com>
2025-03-13 08:50:50 +0100

    Done chapter 2 lesson 1

--- 2_rust/cli/target/debug/cli.d
+++
@@ -1,1 +0,0 @@
-/Users/mariolazzari/Code/Corsi/LinkedIn/choosing-backend/2_rust/cli/target/debug/cli: /Users/mariolazzari/Code/Corsi/LinkedIn/choosing-backend/2_rust/cli/main.rs
--- 2_rust/cli/target/debug/deps/cli-093301cb299defa4.d
+++
@@ -1,5 +0,0 @@
-/Users/mariolazzari/Code/Corsi/LinkedIn/choosing-backend/2_rust/cli/target/debug/deps/cli-093301cb299defa4: main.rs
-
-/Users/mariolazzari/Code/Corsi/LinkedIn/choosing-backend/2_rust/cli/target/debug/deps/cli-093301cb299defa4.d: main.rs
-
-main.rs:
